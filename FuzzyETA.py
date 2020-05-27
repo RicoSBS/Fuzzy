aaa527f51a9b04f2811d44ec77a793a8f34ef216
RicoSBS <ricosetiono@yahoo.co.id>
2020-05-27 20:49:46 +0700

    Fungsi Fuzzy

--- FuzzyETA.py
+++ FuzzyETA.py
@@ -9,6 +9,57 @@ from urllib.request import FancyURLopener
 from datetime import datetime
 
 
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+        home_coord = "-7.7545807,110.37433493" # Home
+        work_coord2 = str(self.path) # Work
+
+        lat = ""
+        lon = ""
+        for i in range(0,len(work_coord2)):
+            if i >= 6 and i<=16:
+                lat += work_coord2[i]
+            elif i >= 22 and i<= 32:
+                lon += work_coord2[i]
+        #print(lat)
+        #print(lon)
+
+        #work_coord = lat,lon
+        #print(work_coord)
+
+        API1 = "AIzaSyCRXi115Ca-2IO6R03VpfYZq6ufvZ9rTcs"
+        url_work2home = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + lat + "," + lon + "&destinations=" + home_coord + "&mode=driving&traffic_model=best_guess&departure_time=now&language=en-EN&sensor=false&key=" + API1
+
+        class MyOpener(FancyURLopener):
+            version = 'My new User-Agent'
+        myopener = MyOpener()
+        result_work2home = simplejson.load(myopener.open(url_work2home))
+        driving_time_seconds_work2home = result_work2home['rows'][0]['elements'][0]['duration_in_traffic']['value']
+        print(datetime.now().strftime('%Y-%m-%d %H:%M') + ";" + str(driving_time_seconds_work2home) + " seconds;" + "\n")
+        
+        target = open("Results.csv", 'a')
+        target.write( datetime.now().strftime('%Y-%m-%d %H:%M') + ";" + str(driving_time_seconds_work2home) + ";"+ "\n")
+        target.close()
+
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
 def Fuzzy():
 	# The universe of variables and membership functions
 	ETA = ctrl.Antecedent(np.arange(-1, 21, 0.1), 'ETA')
@@ -252,58 +303,6 @@ def Fuzzy():
 	"=================================================================================="
 
 
-class S(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-    def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
-
-        home_coord = "-7.7545807,110.37433493" # Home
-        work_coord2 = str(self.path) # Work
-
-        lat = ""
-        lon = ""
-        for i in range(0,len(work_coord2)):
-            if i >= 6 and i<=16:
-                lat += work_coord2[i]
-            elif i >= 22 and i<= 32:
-                lon += work_coord2[i]
-        #print(lat)
-        #print(lon)
-
-        #work_coord = lat,lon
-        #print(work_coord)
-
-        API1 = "AIzaSyCRXi115Ca-2IO6R03VpfYZq6ufvZ9rTcs"
-        url_work2home = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + lat + "," + lon + "&destinations=" + home_coord + "&mode=driving&traffic_model=best_guess&departure_time=now&language=en-EN&sensor=false&key=" + API1
-
-        class MyOpener(FancyURLopener):
-            version = 'My new User-Agent'
-        myopener = MyOpener()
-        result_work2home = simplejson.load(myopener.open(url_work2home))
-        driving_time_seconds_work2home = result_work2home['rows'][0]['elements'][0]['duration_in_traffic']['value']
-        print(datetime.now().strftime('%Y-%m-%d %H:%M') + ";" + str(driving_time_seconds_work2home) + " seconds;" + "\n")
-        
-        target = open("Results.csv", 'a')
-        target.write( datetime.now().strftime('%Y-%m-%d %H:%M') + ";" + str(driving_time_seconds_work2home) + ";"+ "\n")
-        target.close()
-
-
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
-
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
-
-
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
@@ -312,6 +311,7 @@ def run(server_class=HTTPServer, handler_class=S, port=8080):
 
     try:
         httpd.serve_forever()
+        Fuzzy()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
@@ -322,10 +322,8 @@ if __name__ == '__main__':
 
     if len(argv) == 2:
         run(port=int(argv[1]))
-        Fuzzy()
     else:
         run()
-        Fuzzy()
 
 
 "================================================================================="
